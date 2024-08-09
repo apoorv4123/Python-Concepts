d9f4347685619130ac4302b5e0ffa53f67e9dd04
Apoorv Srivastava <apoorvsrivastava@Apoorvs-MacBook-Air.local>
2024-08-09 16:19:12 +0530

    initial commit

---
+++ FirstPythonProgram.py
@@ -0,0 +1,93 @@
+# print("My name is")
+# print("Apoorv Srivastava")
+# print("My name is","Apoorv Srivastava")
+
+# print(23)
+# print(23+35)
+
+# --------------------------------------------------
+
+# variable is a name given to a memory location in a program
+# variable names should be simple, short and meaningful
+
+# Identifiers (They are names...eg. a variable name or a function name, etc. is an identifier):
+# Identifiers can be a combination of uppercase & lowercase letters, digits or an underscore.
+# Identifiers cannot start with a digit
+# Identifiers can be of any length
+# We can't use special symbols in our identifiers like !,@#$%^&*()-+=
+
+
+name = "Ajit"
+age = 22
+price = 25.66
+
+# print(name)
+# print(age)
+# print(price)
+# print("My name is : ", name, ", my age is : ", age)
+
+age2 = age
+# print(age2)
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# Data types:
+# Python automatically detects the type(datatype) of values
+# Data Types in Python : Integer(int), String(str), Float(float), Boolean(bool), None(NoneType)
+# String can be writtn in single quotes, double quotes and triple quotes
+# Possible vaues of boolean = True/False
+# Eg: a = None (means a ko koi value nhi di gyi)
+
+name1 = 'as'
+name2 = "as" #(preferable)
+name3 = '''as'''
+# print(name1, name2, name3)
+
+flag = False
+a = None
+# print(type(flag))
+# print(type(a))
+
+# Keywords:
+# Keywords are reserver words which cannot be used as identifiers
+# and, as, assert, break, class, continue, def, del, elif, else, except, finally, False, for, from, global, if, import, in, is, lambda, nonlocal, None, not, or, pass, raise, return, True, try, with, while, yield
+
+# Python is a case sensetive language
+
+
+# Print Sum:
+a = 2
+b = 5
+sum = a + b
+print(sum)
+
+a = 3.4
+sum = a + b
+print(sum)
+
+b = False
+sum = a + b
+print(sum)
+
+a = "No"
+print(sum)
+
+
+#  Comments: Non executable text
+# 1. Single line comments start with #
+# 2. Multi line comments are enclosed between {"""......."""}
+"""
+multi-line 
+comment
+"""
+
+"""
+Operators(Symbol that performs certain operations between operands):
+Arithematic Operator: +,-,/,*,%,**
+Relational/Comparison operators: ==, !=, >, <, >=, <=
+Assignment Operators: =, +=, -=, *=, /=, %=, **=
+Logical Operators: not, and, or
+"""
+
