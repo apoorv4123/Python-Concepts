75df54d5f6f50e9cff6761d6255473d0a9560207
Apoorv Srivastava <apoorvsrivastava@Apoorvs-MacBook-Air.local>
2024-08-10 14:59:26 +0530

    datatypes, operators, type-casting

--- FirstPythonProgram.py
+++ FirstPythonProgram.py
@@ -58,21 +58,24 @@ a = None
 
 
 # Print Sum:
-a = 2
-b = 5
-sum = a + b
-print(sum)
+# a = 2
+# b = 5
+# sum = a + b
+# print(sum)
 
-a = 3.4
-sum = a + b
-print(sum)
+# a = 3.4
+# sum = a + b
+# print(sum)
 
-b = False
-sum = a + b
-print(sum)
+# https://www.geeksforgeeks.org/type-conversion-in-python/
+# b = False 
+# print(type(b))
+# sum = a + b
+# print(sum)
 
-a = "No"
-print(sum)
+# a = "No"
+# print(type(a))
+# print(sum)
 
 
 #  Comments: Non executable text
@@ -83,11 +86,82 @@ multi-line
 comment
 """
 
+
+# Operators(Symbol that performs certain operations between operands):
 """
-Operators(Symbol that performs certain operations between operands):
-Arithematic Operator: +,-,/,*,%,**
-Relational/Comparison operators: ==, !=, >, <, >=, <=
+Arithematic Operators -> For mathematical operations: +,-,/,*,%,**
+Relational/Comparison operators -> Return a boolean value : ==, !=, >, <, >=, <=
 Assignment Operators: =, +=, -=, *=, /=, %=, **=
-Logical Operators: not, and, or
+Logical Operators -> Operate on boolean values : not, and, or
 """
 
+'''
+a = 4
+b = 2
+
+sum = a + b # assigning sum to a variable
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) # remainder
+print(a**b) # power
+print(a+False) # just fyi
+
+print(a == b)
+print(a != b)
+print(a >= b)
+print(a > b)
+print(a <= b)
+print(a < b)
+
+num = 10
+num += 10 # num = num + 10
+print(num)
+
+print(not False)
+print(not True)
+print(not (a > b))
+
+val1 = True
+val2 = False
+print("and operator:", val1 and val2)
+print("or operator:", val1 or val2)
+print((a == b) or (a > b))
+'''
+
+#  Type Conversion : Converting the variable of one type to another 
+'''
+2 types of conversions are there in Python:
+1. Type Conversion/Implicit: Python interpreter does ths automatically
+2. Type Casting/Explicit:  We need to inform python that we are converting vatianle of one type of variable of another type
+'''
+
+"""
+a = 2 # int
+b = 4.25 # float
+
+sum = a + b #When we are doing arithematic operations between int and float, python would automatically convert int to float(as float is superior as compared to int) as float can store more/extra data 
+# So, above: sum = 2.0 + 4.25 -> Python automatically would've performed type
+
+a1 = "2"
+print(a1 + b) # Error as we can only concatenate str(not float) to str. Here python did not do automatic conversion
+"""
+
+'''
+# Type Casting is done using several functions like: int(value) -> this would give out value as int, float(value) -> this would give out value as float, str(value)
+a,b = 1,"2"
+c = int(b)
+d = float(b)
+sum = a + c
+print(sum)
+sum = a + d
+print(sum)
+#  only numerical strings can be converted to float or int using type casting, we can't convert a character string to float value
+
+a = 3.14
+a = str(a)
+print(type(a))
+'''
+
+# Input in python: Taking input from user in python
